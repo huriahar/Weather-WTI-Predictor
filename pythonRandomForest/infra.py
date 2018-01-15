997cca27f3ff1cc5fd7e86b30745ea6515f9fdd4
huriahar <harshita.huria@mail.utoronto.ca>
2018-01-15 12:23:51 -0500

    Add date and past data support in infra
    Customize start date for test data and how many days of training data you wish to use

--- pythonRandomForest/infra.py
+++ pythonRandomForest/infra.py
@@ -24,10 +24,6 @@ def read_weather_data (file_name):
     df.columns.values[0] = date_column
     return df
 
-def read_csv (file_name):
-    df = pd.read_csv(file_name)
-    return df
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--future', nargs='?', const=0, type=int,
                     help="Add the number of days from the given day that you wish to make the prediction for")
@@ -37,12 +33,11 @@ parser.add_argument('-p', '--past', nargs='?', const=0, type=int,
                     help="Use past # of days starting from 'date' as training data")
 args = parser.parse_args()
 
-future_days = args.future
-if args.date:
-    date = pd.to_datetime(str(args.date), format='%Y%m%d')
-    print(date)
-past_days = args.past
-print(past_days)
+# TODO: Add error checks for each of the inputs supplied
+future_days = 0 if args.future is None else args.future
+date = 0 if args.date is None else pd.to_datetime(str(args.date), format='%Y%m%d')
+past_days = 0 if args.past is None else args.past
+print(past_days, date, future_days)
 
 houston_data = read_weather_data("houston_weather.csv")
 dhahran_data = read_weather_data("dhahran_weather.csv")
@@ -63,7 +58,7 @@ merged_weather.fillna(merged_weather.mean()['houston_temperature':'omsk_precipit
 
 # merged_weather = pd.read_csv("merged_weather.csv")
 
-WTI_data = read_csv("DCOILWTICO.csv")
+WTI_data = pd.read_csv("DCOILWTICO.csv")
 # print(WTI_data.isna().sum())
 
 # Merge both dataframes and use only the dates which are available for both weather and WTI
@@ -79,14 +74,32 @@ merged_data.set_index('Date', inplace=True)
 # merged_data.to_csv('merged_data.csv')
 
 # Next step is to offset so you align how many days you wish to predict into the future with the present day!
-if future_days is not None:
-    merged_data.DCOILWTICO = merged_data.DCOILWTICO.shift(-1*future_days)
-    # Last future_days rows contain NaN for DCOILWTICO...
-    # TODO: Figure out what to do with that - Just ignore when splitting train and test
+merged_data.DCOILWTICO = merged_data.DCOILWTICO.shift(-1*future_days)
+# Last future_days rows contain NaN for DCOILWTICO...
+# TODO: Figure out what to do with that - Just ignore when splitting train and test
 
+# Now, if date is supplied, start prediction from the given date. If not, use default separator at 80% of total data
+# If past is supplied, use only the past number of days before given date for training. If not, use all the data before the "date"
+
+# TODO: Add edge case checks!!! Now just assuming that if both date and past is supplied, you have plenty of training data available
+# This will break if you supply random values! Check!!!
+if date in merged_data.index:
+    print(merged_data.loc[date, :])
+    print("Location idx:", merged_data.index.get_loc(date))
+    start_test_idx = merged_data.index.get_loc(date)
+else:
+    start_test_idx = int((len(merged_data) - future_days)*0.8)
+
+# TODO: Add error checks for past # being out of bounds
+if past_days != 0 and (start_test_idx - past_days) >= 0:
+    start_train_idx = start_test_idx - past_days
+else:
+    start_train_idx = 0
+
+print(start_train_idx, start_test_idx)
 # Time Series split of train and test data 80-20
-train_size = int((len(merged_data)-future_days)*0.8)
-train, test = merged_data[0:train_size], merged_data[train_size:(len(merged_data)-future_days)]
+
+train, test = merged_data[start_train_idx:start_test_idx], merged_data[start_test_idx:(len(merged_data)-future_days)]
 print("Total observations: %d" % (len(merged_data) - future_days))
 print("Training observations: %d" % len(train))
 print("Testing observations: %d" % len(test))
@@ -106,5 +119,3 @@ print("R squared:", r2)
 
 test['DCOILWTICO'].plot(figsize=(16,12))
 test['PredWTI'].plot(figsize=(16,12))
-
-
